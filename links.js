086307eb526b8df2ea8b5fe18fbac2bd80e270a3
14569 <165989521+kk14569@users.noreply.github.com>
2024-08-26 18:52:47 +0800

    Create links.js

---
+++ links.js
@@ -0,0 +1,39 @@
+const links = [
+    {
+        title: "145Hub",
+        url: "https://hub.00.uy",
+        description: "热爱闲聊、分享的社区平台。",
+        tags: [
+            { tag: "社区" },
+            { tag: "分享" }
+        ]
+    },
+    {
+        title: "NiceForum",
+        url: "https://niceforum.freeflarum.com",
+        description: "Nice 论坛是一个交流各种东西的论坛。",
+        tags: []
+    },
+    {
+        title: "dfgg 论坛",
+        url: "https://bbs.dfggmc.top",
+        description: "dfgg 论坛 | 不止 MC",
+        tags: [
+            { tag: "MC" }
+        ]
+    },
+    {
+        title: "Block BBS",
+        url: "https://block.freeflarum.com",
+        description: "一个Minecraft玩家聚集地，不只是Minecraft。",
+        tags: [
+            { tag: "MC" }
+        ]
+    },
+    {
+        title: "Note.msBBS",
+        url: "https://bbs.notems.xyz",
+        description: "在这里聊聊天或者备个份。",
+        tags: []
+    },
+];
